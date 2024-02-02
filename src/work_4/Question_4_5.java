cdb9b00e45b2c1d18f1babde0bfd575d116093e2
HaoXiang <karta0128@gmail.com>
2024-02-02 23:49:40 +0800

    修改Qusetion_4_5的閱讀性

--- src/work_4/Question_4_5.java
+++ src/work_4/Question_4_5.java
@@ -9,18 +9,16 @@ public class Question_4_5 {
 
 	// 天數計算使用
 	public void toDay(int[] date) {
+		int[] monthDay = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 		int sum = 0;
-		sum += (date[1] - 1) * 30;
-		sum = sum + date[1] / 2;
-		if (date[1] > 7)
-			sum++;
-		if (date[1] >= 2) {
-			if (leapYear(date[0]))
-				sum = sum - 1;
-			else
-				sum = sum - 2;
+		for (int i = 1; i < date[1]; i++)
+			sum += monthDay[i];
+		if (date[1] > 2) {
+			if (leapYear(date[0])) {
+				sum++;
+			}
 		}
-		System.out.println(sum + date[2]);
+		System.out.println("此為 " + date[0] + " 年的第 " + (sum + date[2]) + " 天");
 	}
 
 	// 檢查使用者輸入內容是否有錯誤
@@ -28,11 +26,11 @@ public class Question_4_5 {
 		int[] date = new int[3];
 		for (int i = 0; i < date.length; i++) {
 			if (i == 0)
-				System.out.println("請輸入年分：");
+				System.out.print("請輸入年份：");
 			if (i == 1)
-				System.out.println("請輸入月份");
+				System.out.print("\n請輸入月份：");
 			if (i == 2)
-				System.out.println("請輸入日期");
+				System.out.print("\n請輸入日期：");
 			date[i] = sc.nextInt();
 			if (date[1] >= 13) {
 				System.out.println("月份輸入錯誤，請重新輸入!");
@@ -44,7 +42,7 @@ public class Question_4_5 {
 					i = 1;
 				} else {
 					if (date[2] > 29 || date[2] < 0) {
-						System.out.println("這個年分的二月只有28天，請重新輸入");
+						System.out.println("這個年份的二月只有28天，請重新輸入");
 						i = 1;
 					}
 				}
@@ -57,7 +55,6 @@ public class Question_4_5 {
 				System.out.println("你輸入的月份天數是錯誤的" + date[1] + "月，最多只有30天，請重新輸入");
 				i--;
 			}
-
 		}
 		toDay(date);
 	}
